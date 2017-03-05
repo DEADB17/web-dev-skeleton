e4fd9bb67c8bff3655ed87627aa01a6639b269b5
DEADB17 <DEADB17@gmail.com>
2017-03-05 04:11:18 -0500

    replace ava with tape

--- src/test/index.js
+++ src/test/index.js
@@ -1,20 +1,8 @@
-/* yarn test -- <options>
- * Options
- * --init             Add AVA to your project
- * --fail-fast        Stop after first test failure
- * --serial, -s       Run tests serially
- * --tap, -t          Generate TAP output
- * --verbose, -v      Enable verbose output
- * --no-cache         Disable the transpiler cache
- * --no-power-assert  Disable Power Assert
- * --match, -m        Only run tests with matching title (Can be repeated)
- * --watch, -w        Re-run tests when tests and source files change
- * --source, -S       Pattern to match source files so tests can be re-run (Can be repeated)
- * --timeout, -T      Set global timeout
- */
+/* eslint-env commonjs */
 
-import test from 'ava';
+const test = require('tape');
 
 test('default', t => {
     t.fail('add tests');
+    t.end();
 });
