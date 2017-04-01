8848d166ec278433df5c400da4a2bbe2b944204c
DEADB17 <DEADB17@gmail.com>
2017-04-01 19:59:49 -0400

    remove rollup-plugin-buble as buble is not used yet

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
-import buble from 'rollup-plugin-buble';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     plugins: [
-        buble(),
         resolve({
             jsnext: true,
             main: true,
